b89e376bb1627f5be2250ad3257716ae26472dbe
Alexandre Aimonino <a.aimonino@koffy.finance>
2024-06-21 13:50:40 +0200

    feat(terraform): added second host and defined hostnames

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,31 +1,61 @@
 provider "aws" {
-  region = "eu-central-1" # Remplacez par la région de votre choix
+  region = "eu-central-1"
 }
 
-resource "aws_instance" "my_instance" {
-  ami           = "ami-09042b2f6d07d164a" # Remplacez par l'AMI de votre choix
-  instance_type = "m5.xlarge" # Type d'instance avec 4 vCPU et 16 Go de RAM
+# Instance and volume for Nimbus
+resource "aws_instance" "nimbus_instance" {
+  ami           = "ami-09042b2f6d07d164a"
+  instance_type = "m5.xlarge"
 
   root_block_device {
     volume_size = 10 # Root volume
   }
 
   tags = {
-    Name = "aws-holesky-nethermind-nimbus-001"
+    Name = "aws-holesky-nimbus-001"
   }
 }
 
-resource "aws_ebs_volume" "data_volume" {
-  availability_zone = aws_instance.my_instance.availability_zone
+resource "aws_ebs_volume" "nimbus_data_volume" {
+  availability_zone = aws_instance.nimbus_instance.availability_zone
   size              = 200 # 200Go for block-volume
 
   tags = {
-    Name = "chain-block-volume"
+    Name = "nimbus-chain-block-volume"
   }
 }
 
-resource "aws_volume_attachment" "ebs_att" {
-  device_name = "/dev/sdb" # Peut varier selon le système d'exploitation
-  volume_id   = aws_ebs_volume.data_volume.id
-  instance_id = aws_instance.my_instance.id
+resource "aws_volume_attachment" "nimbus_ebs_att" {
+  device_name = "/dev/sdb"
+  volume_id   = aws_ebs_volume.nimbus_data_volume.id
+  instance_id = aws_instance.nimbus_instance.id
+}
+
+# Instance and volume for Nethermind
+resource "aws_instance" "nethermind_instance" {
+  ami           = "ami-09042b2f6d07d164a"
+  instance_type = "m5.xlarge"
+
+  root_block_device {
+    volume_size = 10 # Root volume
+  }
+
+  tags = {
+    Name = "aws-holesky-nethermind-001"
+  }
+}
+
+resource "aws_ebs_volume" "nethermind_data_volume" {
+  availability_zone = aws_instance.nethermind_instance.availability_zone
+  size              = 200 # 200Go for block-volume
+
+  tags = {
+    Name = "nethermind-chain-block-volume"
+  }
+}
+
+resource "aws_volume_attachment" "nethermind_ebs_att" {
+  device_name = "/dev/sdb"
+  volume_id   = aws_ebs_volume.nethermind_data_volume.id
+  instance_id = aws_instance.nethermind_instance.id
 }
